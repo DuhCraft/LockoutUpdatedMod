f7309a1f9203a88191e9f823593dbe6fc3629441
Craft <rkschmidt.cse@gmail.com>
2026-01-02 15:40:16 -0600

    commit CraftItemGoal class which is the type of crafted goal

--- src/main/java/com/craft/lockoutmod/goal/goals/CraftItemGoal.java
+++ src/main/java/com/craft/lockoutmod/goal/goals/CraftItemGoal.java
@@ -1,4 +1,32 @@
 package com.craft.lockoutmod.goal.goals;
 
-public class CraftItemGoal {
+import com.craft.lockoutmod.goal.GoalCategory;
+import com.craft.lockoutmod.goal.LockoutGoal;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+
+public class CraftItemGoal extends LockoutGoal {
+
+    private final Item targetItem;
+
+    public CraftItemGoal(String id, Item targetItem){
+        super(id,
+                GoalCategory.ITEMS,
+                Component.literal("Craft ").append(new ItemStack(targetItem).getHoverName()),
+                new ItemStack(targetItem)
+        );
+        this.targetItem = targetItem;
+
+    }
+
+    @Override
+    public boolean matches(Object event) {
+        if(!(event instanceof PlayerEvent.ItemCraftedEvent crafted)){
+            return false;
+        }
+
+        return crafted.getCrafting().is(targetItem);
+    }
 }
